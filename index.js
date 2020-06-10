699387d323f7cf3d264f2da168a415f69f18fe16
Carley Tripp <carley.tripp@outlook.com>
2020-06-10 04:31:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const k_muppet = {
   k_partner: 'Miss Piggy'
 };
 
+
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
@@ -33,17 +34,42 @@ const k_muppet = {
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
 
+const { moo, neigh, ba, oink, cluck } = farmAnimals.split(" ")
+const { Bessie, , Dolly, Babe, Little } = farmAnimals.split(" ")
+const { black_and_white, , black, pink, } = farmAnimals.split(" ")
+
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+
+const { red, orange, yellow, green, blue, indigo, violet } = colors
+const { r, o, y, g, b, , v } = colors
+const { , , , , , indg , } = colors
 
 // Objects
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
+const { name, color, song, job, partner } = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+// const k_muppet = {
+//   k_name: 'Kermit',
+//   k_color: 'green',
+//   k_album: {
+//     the_muppet_movie: {
+//       song_1: 'Rainbow Connection',
+//       song_2: 'Moving Right Along',
+//       song_3: 'Never Before, Never Again',
+//       song_4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   k_job: 'Host of The Muppet Show',
+//   k_partner: 'Miss Piggy'
+// };
+const { , song_2, , song_4 } = k_muppet.k_album.the_muppet_movie
+const { , , , job, partner } = k_muppet
